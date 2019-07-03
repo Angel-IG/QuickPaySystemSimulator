c06e7fd08a36babe63ee35de8e2ad20980068368
Angel_I_G <angel.ibagut@gmail.com>
2019-07-03 18:54:16 +0200

    Improving the style and the functionality. TODO: Adding logic

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let nRows;
 document.body.onload = () => {
-  do {
-    nRows = prompt('Insert the number of rows the supermarket have. Please enter an integer number:');
-  } while (isNaN(nRows) || nRows === 0);
+  nRows = parseInt(prompt('Insert the number of rows the supermarket have. Please enter an integer number:'));
+  while (isNaN(nRows) || parseInt(nRows) <= 0) {
+    nRows = parseInt(prompt('Invalid input. Please enter an integer number. This number will be the number of rows of the supermarket:'));
+  }
+  document.getElementById('paragraphs').innerHTML += '<p>This supermarket have ' + nRows + ' rows.</p>';
 };
